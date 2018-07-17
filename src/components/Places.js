6d329846314f79f5247d15a3cc62dc1498c08f53
dragosbus <dragosbusuioc94@gmail.com>
2018-07-17 14:17:35 +0200

    flatlist

--- src/components/Places.js
+++ src/components/Places.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import ListItem from './ListItem';
 
 const Places = props => {
-  return <View style={styles.places}>{props.places.map((place, i) => <ListItem key={i} placeName={place} onItemPressed={()=>props.onItemDelete(i)}/>)}</View>;
+  return <FlatList style={styles.places}>{props.places.map((place, i) => <ListItem key={i} placeName={place} onItemPressed={()=>props.onItemDelete(i)}/>)}</FlatList>;
 };
 
 const styles = StyleSheet.create({
     places: {
         marginTop: 20,
-        width: '100%',
-        alignItems: 'center'
+        width: '100%'
     }
 });
 
