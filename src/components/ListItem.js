6f8ba0f5e468c96f202b0c3b5f32d0b7d3afb8d1
dragosbus <dragosbusuioc94@gmail.com>
2018-07-17 13:39:02 +0200

    styled the list with places

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,14 +4,21 @@ import {View, Text, StyleSheet} from 'react-native';
 const ListItem = (props) =>{
     return(
         <View style={styles.listItem}>
-            <Text>{props.placeName}</Text>
+            <Text style={styles.placeText}>{props.placeName}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     listItem: {
-        width: '100%'
+        width: '90%',
+        backgroundColor: '#e6e6e6',
+        padding: 10,
+        borderRadius: 4,
+        marginTop: 10
+    },
+    placeText: {
+        fontSize: 16
     }
 });
 
--- src/components/Places.js
+++ src/components/Places.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import ListItem from './ListItem';
 
 const Places = props => {
-  return <View>{props.places.map((place, i) => <ListItem key={i} placeName={place} />)}</View>;
+  return <View style={styles.places}>{props.places.map((place, i) => <ListItem key={i} placeName={place} />)}</View>;
 };
+
+const styles = StyleSheet.create({
+    places: {
+        marginTop: 20,
+        width: '100%',
+        alignItems: 'center'
+    }
+});
 
 export default Places;
